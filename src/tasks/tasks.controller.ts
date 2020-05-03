cdf8dbf77bf6d293f5d1a15e58aea8e7128b38b1
Andrea Bender <andreab.1726@gmail.com>
2020-05-03 15:31:50 -0600

    refactored patch/update

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 
 @Controller('tasks')
@@ -34,11 +35,11 @@ export class TasksController {
     return this.tasksService.deleteTask(id);
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  //   ): Task {
-  //   return this.tasksService.updateTaskStatus(id, status)
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status)
+  }
 }
